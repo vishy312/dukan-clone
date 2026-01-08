7c8149cd160bc156e52a62c3cc9833ae47340dc0
vishalpalDI <vishal.p@diverseinfotech.com>
2026-01-08 11:51:55 +0530

    subitems added in sidebar

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -7,6 +7,7 @@ import { navItems } from "../utils/navitems";
 
 const Sidebar = () => {
   const [selected, setSelected] = useState("");
+  const [selectedSubitem, setSelectedSubitem] = useState("");
 
   return (
     <div className="border relative border-neutral-900 w-1/7 min-h-screen bg-[hsl(225,34,16)] text-neutral-200 p-3 font-sans">
@@ -19,21 +20,40 @@ const Sidebar = () => {
           const Icon = item.icon;
           const ActiveIcon = item.activeIcon;
           return (
-            <div
-              key={index}
-              className={`p-2 flex gap-2 cursor-pointer hover:bg-neutral-50/5 rounded-sm ${
-                selected === item.title ? "bg-neutral-50/10" : ""
-              }`}
-              onClick={() => setSelected(item.title)}
-            >
-              {selected === item.title && <ActiveIcon />}
-              {selected !== item.title && <Icon />}
-              <span className="text-xs">{item.title}</span>
-            </div>
+            <>
+              <div
+                key={index}
+                className={`p-2 flex gap-2 text-neutral-400 cursor-pointer hover:bg-neutral-50/5 rounded-sm ${
+                  selected === item.title ? "bg-neutral-50/10" : ""
+                }`}
+                onClick={() => {
+                  setSelected(item.title);
+                  setSelectedSubitem("");
+                }}
+              >
+                {selected === item.title && <ActiveIcon />}
+                {selected !== item.title && <Icon />}
+                <span className="text-xs text-neutral-300">{item.title}</span>
+              </div>
+              <ul className="mx-6 py-1 list-none text-[10px] flex flex-col text-neutral-400 ">
+                {item.title === selected &&
+                  item.subItems.map((subitem, ind) => (
+                    <li
+                      key={ind}
+                      className={`cursor-pointer hover:bg-neutral-50/5 rounded-sm px-2 py-1 ${
+                        selectedSubitem === subitem ? "text-neutral-200" : ""
+                      }`}
+                      onClick={() => setSelectedSubitem(subitem)}
+                    >
+                      {subitem}
+                    </li>
+                  ))}
+              </ul>
+            </>
           );
         })}
       </div>
-      <div className="flex gap-3 items-start bg-neutral-50/10 rounded-md p-2 absolute bottom-3 left-3 right-3">
+      <div className="flex gap-3 items-start bg-neutral-50/10 rounded-md p-2  bottom-3 left-3 right-3">
         <RiWallet3Line className="size-8 text-neutral-100 p-1 bg-neutral-50/20 rounded-sm aspect-square" />
         <div className="text-xs p-0 flex flex-col justify-start ">
           <span className="text-neutral-400">Available credits </span>
